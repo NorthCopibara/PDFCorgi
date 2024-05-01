95cb3384a06f987997c4e642b57bb74784cd00e1
NorthCapybara <INorthCapibara@gmail.com>
2024-05-01 15:12:11 +0300

    configs

--- Resources/Config.py
+++ Resources/Config.py
@@ -1,3 +1,3 @@
-PDF_CONFIG_PATH = '../Resources/PdfConfig.csv'
+PDF_CONFIG_PATH = 'PdfConfig_avangard_4_teen.csv'
 PDF_CONFIG_PATH_AVANGARD_4 = '../Resources/PdfConfig_avangard_4.csv'
 SAVE_DATA_PATH = '../Resources/SavedPath.txt'
